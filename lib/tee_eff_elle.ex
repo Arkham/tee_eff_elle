8d80f25bbf7358280b782f776393513a1c84202e
Ju Liu <ju@noredink.com>
2019-10-27 12:15:19 +0000

    Initial commit

---
+++ lib/tee_eff_elle.ex
@@ -0,0 +1,18 @@
+defmodule TeeEffElle do
+  @moduledoc """
+  Documentation for TeeEffElle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TeeEffElle.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tee_eff_elle_test.exs
@@ -0,0 +1,8 @@
+defmodule TeeEffElleTest do
+  use ExUnit.Case
+  doctest TeeEffElle
+
+  test "greets the world" do
+    assert TeeEffElle.hello() == :world
+  end
+end
